d55c0c5f642842070030daae74bf9d435bc967b2
Moodrammer <mahmou.m.gody@gmail.com>
2023-03-12 21:32:37 +0200

    Add: snake assets

--- snake.py
+++ snake.py
@@ -12,14 +12,30 @@ class SNAKE:
         self.body_cells = [Vector2(5,10), Vector2(4, 10), Vector2(3, 10)]
         self.is_add_new_block = False
         
+        self.snake_head_right_asset = pygame.image.load('Graphics/snake_head_R.png')
+        self.snake_head_left_asset = pygame.image.load('Graphics/snake_head_L.png')
+        self.snake_head_up_asset = pygame.image.load('Graphics/snake_head_U.png')
+        self.snake_head_down_asset = pygame.image.load('Graphics/snake_head_D.png')
+        
+        self.snake_body_asset = pygame.image.load('Graphics/snake_body.png')
+        
     def draw_snake(self, display):
         head_rect = pygame.Rect(int(self.get_head_pos().x * self.cell_size), int(self.get_head_pos().y * self.cell_size), self.cell_size, self.cell_size)
-        pygame.draw.rect(display, pygame.Color('black'), head_rect)
+        # pygame.draw.rect(display, pygame.Color('black'), head_rect)
+        if self.direction == DIRECTIONS['RIGHT']:
+            display.blit(self.snake_head_right_asset, head_rect)
+        if self.direction == DIRECTIONS['LEFT']:
+            display.blit(self.snake_head_left_asset, head_rect)
+        if self.direction == DIRECTIONS['DOWN']:
+            display.blit(self.snake_head_down_asset, head_rect)
+        if self.direction == DIRECTIONS['UP']:
+            display.blit(self.snake_head_up_asset, head_rect)
         
         for i in range(1, len(self.body_cells)):
             body_cell = self.body_cells[i]
             body_cell_rect = pygame.Rect(int(body_cell.x * self.cell_size), int(body_cell.y * self.cell_size), self.cell_size, self.cell_size)
-            pygame.draw.rect(display, self.snake_color, body_cell_rect)
+            # pygame.draw.rect(display, self.snake_color, body_cell_rect)
+            display.blit(self.snake_body_asset, body_cell_rect)
             
     def _is_reverse_direction(self, dir1, dir2):
         if (dir1 == DIRECTIONS['LEFT'] and dir2 == DIRECTIONS['RIGHT']) or (dir1 == DIRECTIONS['RIGHT'] and dir2 == DIRECTIONS['LEFT']):
