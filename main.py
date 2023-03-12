e7d537fb21d70c6f83bd193f4c36c066d8db6f22
Moodrammer <mahmou.m.gody@gmail.com>
2023-03-12 16:58:29 +0200

    Add: Game class

--- main.py
+++ main.py
@@ -13,12 +13,25 @@ cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number)) # creating a display surface
 clock = pygame.time.Clock()
-f = FRUIT(cell_size, cell_number)
-s = SNAKE(cell_size, cell_number)
 
 # Game User-defined events
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+# Main game class
+class GAME:
+    def __init__(self):
+        self.fruit = FRUIT(cell_size, cell_number)
+        self.snake = SNAKE(cell_size, cell_number)
+        
+    def update(self):
+        self.snake.move_snake()
+   
+    def draw_elements(self):
+        self.fruit.draw_fruit(screen)
+        self.snake.draw_snake(screen)    
+
+game = GAME()
 
 while True:
     for event in pygame.event.get():
@@ -26,21 +39,20 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            s.move_snake()
+            game.update()  
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                s.set_snake_direction(DIRECTIONS['UP'])
+                game.snake.set_snake_direction(DIRECTIONS['UP'])
             if event.key == pygame.K_DOWN:
-                s.set_snake_direction(DIRECTIONS['DOWN'])
+                game.snake.set_snake_direction(DIRECTIONS['DOWN'])
             if event.key == pygame.K_LEFT:
-                s.set_snake_direction(DIRECTIONS['LEFT'])
+                game.snake.set_snake_direction(DIRECTIONS['LEFT'])
             if event.key == pygame.K_RIGHT:
-                s.set_snake_direction(DIRECTIONS['RIGHT'])
+                game.snake.set_snake_direction(DIRECTIONS['RIGHT'])
         
     
     screen.fill((175, 215, 70))
-    f.draw_fruit(screen)
-    s.draw_snake(screen)
+    game.draw_elements()
     
     pygame.display.update()
     clock.tick(60) # Running at 60 fps as a maximum rate
